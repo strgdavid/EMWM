6746e176ac835a9b87bcde97e973757237bce53e
strgdavid <78654402+strgdavid@users.noreply.github.com>
2024-06-21 20:01:35 +0000

    21.06.2024 22:01

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 import json
 import os
 import random
-import datetime
+import re
 
 import discord
 import requests
@@ -61,9 +61,7 @@ async def help(ctx):
 
 @client.command(name='gif')#, pass_context=True)
 async def gif(ctx):
-  #await ctx.send('https://tenor.com/view/emirmono-mittelfinger-gif-791195104924975174')
-  #await ctx.send('https://tenor.com/view/emirmono-mittelfinger-gif-791195104924975174')
-  await ctx.send()
+  await ctx.send('https://tenor.com/view/emirmono-mittelfinger-gif-791195104924975174')
 
 #def is_date_in_range(date, start_date, end_date):
 #   return start_date <= date <= end_date
@@ -76,10 +74,25 @@ async def em(ctx):
   datetime_now = datetime.datetime.now()
   dt2 = datetime.datetime(2023, 6, 20)
   
-  if(datetime.datetime(1999, 1, 1) <= datetime_now <= datetime.datetime(2100, 1, 1)):
-    #print(dt)
+  if (datetime.datetime(1999, 1, 1) <= datetime_now <= datetime.datetime(2024, 6, 22)): #2. Spieltag bis 22.06.2024
     url = 'https://www.fussballdaten.de/em/2024/gruppenphase/2-spieltag/'
-    print('ja, dazwischen') #CHECKPOINT: URL FILTER JE NACH DATUM UND DAMIT AUCH SPIELTAG/XFINALE, DATETIMES EINSTELLEN AUF FILTER
+  elif (datetime.datetime(2024, 6, 23) <= datetime_now <= datetime.datetime(2024, 6, 26)): #3. Spieltag 23.06.2024-26.06.2024
+    url = 'https://www.fussballdaten.de/em/2024/gruppenphase/3-spieltag/'
+  elif (datetime.datetime(2024, 6, 27) <= datetime_now <= datetime.datetime(2024, 7, 2)): #Achtel 29.06.2024-02.07.2024
+    url = 'https://www.fussballdaten.de/em/2024/achtelfinale/'
+  elif (datetime.datetime(2024, 7, 3) <= datetime_now <= datetime.datetime(2024, 7, 6)): #Viertel 05.07.2024-06.07.2024
+    url = 'https://www.fussballdaten.de/em/2024/viertelfinale/'
+  elif (datetime.datetime(2024, 7, 7) <= datetime_now <= datetime.datetime(2024, 7, 10)): #Halb 09.07.2024-10.07.2024
+    url = 'https://www.fussballdaten.de/em/2024/halbfinale/'
+  elif (datetime.datetime(2024, 7, 11) <= datetime_now <= datetime.datetime(2024, 7, 14)): #Finale 14.07.2024
+    url = 'https://www.fussballdaten.de/em/2024/finale/'
+  else:
+    url = 'https://www.fussballdaten.de/em/2024/finale/'
+    
+    #CHECKPOINT: URL FILTER JE NACH DATUM UND DAMIT AUCH SPIELTAG/XFINALE, DATETIMES EINSTELLEN AUF FILTER
+
+    
+  
 
   
   #1. Datum überprüfen
@@ -94,24 +107,73 @@ async def em(ctx):
   
 
   
-  url = 'https://www.fussballdaten.de/em/2024/gruppenphase/2-spieltag/'
+  #url = 'https://www.fussballdaten.de/em/2024/gruppenphase/2-spieltag/'
   html = requests.get(url)
   
   s = BeautifulSoup(html.content, 'html.parser')
 
   results = s.find(id = 'page-content')
   spielplan = results.find(class_ = 'content-spiele')
-  #spiele = spielplan.find(class_ = 'spiele-row detils')
-  n_spiele = len(spielplan.find_all(class_ = 'spiele-row detils'))
+  spiel_titel = spielplan.find(class_ = 'spiele-row detils')
   
-  print(n_spiele)
+  spiel_titel_pl = spielplan.find_all(class_ = 'spiele-row detils')
+
+  spiel_titel_pl_list = list(spiel_titel_pl)
+  
+  ###
+  #spiel_titel_pl = spielplan.find_next(class_ = 'spiele-row detils')
+  #spiel_titel_pl = spiel_titel.find
+  ###
+  
+  n_spiele = len(spielplan.find_all(class_ = 'spiele-row detils'))
+
+
+  for i in range(n_spiele):
+    
+  
+  
+    #result = re.split(r'([A-Za-z]+)(\d+:\d+)(\d+:\d+)([A-Za-z]+)([A-Z])', spiel_titel_pl_list[i].text) #spiel_titel ##########################
+  
+    ###
+    #info_x = ""
+    #for x in range(n_spiele):
+    #  info_x = info_x + f"{result[x]} "
+
+
+    land_1 = ""
+    ergebnis = ""
+    ergebnis_halb = ""
+    land_2 = ""
+    
+    #land_1 = result[1]
+    #ergebnis = result[2]
+    #ergebnis_halb = result[3]
+    #land_2 = result[4]
+  
+    #title_content = spiel_titel_pl_list[i].find(class_='ergebnis')['title'] #spiel_titel
+  
+    #spiel_datum = re.search(r'\((\d{2}\.\d{2}\.\d{4})', title_content).group(1)
+  
+    #nachricht = f"{spiel_datum}: {land_1} gegen {land_2}: {ergebnis} ({ergebnis_halb})"
+    
+    await ctx.send(spiel_titel_pl_list[i].text)
+    #await ctx.send(spiel_titel_2)
+  
+    #print(spiel_titel_pl_list[1].text)
+    
+    #await ctx.send(f"{info_x} {spiel_datum}")
+    
   #for i in range(n_spiele):
-    #await ctx.send("ja")
     
 
   
-  #await ctx.send('test')
-  #print(soup.find_all(attrs={'class':'spiele-row.detils'}))
+
+
+
+
+
+
+  
 
 
 
